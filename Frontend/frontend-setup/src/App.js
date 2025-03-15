40dffa9547671813cf7386c97581815befde4af0
wschen1997 <wschen1997@gmail.com>
2025-03-15 13:13:52 -0400

    Development Update

--- Frontend/frontend-setup/src/App.js
+++ Frontend/frontend-setup/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import FilterPage from "./pages/FilterPage.js";
 import DetailPage from "./pages/DetailPage.js";
+import CrowdfundingPage from "./pages/Crowdfunding.js"; // <-- NEW IMPORT
 import "./App.css";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/filter" element={<FilterPage />} />
           <Route path="/reits/:ticker" element={<DetailPage />} />
+
+          {/* NEW: Crowdfunding route */}
+          <Route path="/Crowdfunding" element={<CrowdfundingPage />} />
         </Routes>
       </Router>
     </div>
