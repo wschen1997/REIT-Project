f21050fdaa1805c5ac386a464f4eb8b121c58d36
wschen1997 <wschen1997@gmail.com>
2025-03-16 11:15:09 -0400

    Development Update

--- Frontend/frontend-setup/src/App.js
+++ Frontend/frontend-setup/src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// App.js
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import * as ReactGaModule from "react-ga4";
+
 import HomePage from "./pages/HomePage.js";
 import FilterPage from "./pages/FilterPage.js";
 import DetailPage from "./pages/DetailPage.js";
-import CrowdfundingPage from "./pages/Crowdfunding.js"; // <-- Crowdfunding list page
-import RecDetailPage from "./pages/RecDetailPage.js";   // <-- NEW IMPORT for the detail page
+import CrowdfundingPage from "./pages/Crowdfunding.js";
+import RecDetailPage from "./pages/RecDetailPage.js";
+
 import "./App.css";
+
+// Log to see exactly what's in ReactGaModule
+console.log("ReactGaModule is:", ReactGaModule);
+console.log("ReactGaModule.default is:", ReactGaModule.default);
+
+// The second .default is your GA4 instance
+const realGA = ReactGaModule.default.default;
+
+// Now you can call .initialize on the real GA object
+const TRACKING_ID = "G-HH9G61RW3G"; // Your GA4 measurement ID
+realGA.initialize(TRACKING_ID);
+
+// Track pageviews on route change
+function AnalyticsTracker() {
+  const location = useLocation();
+  useEffect(() => {
+    realGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <AnalyticsTracker />
         <Routes>
-          {/* Home route => HomePage */}
           <Route path="/" element={<HomePage />} />
           <Route path="/filter" element={<FilterPage />} />
           <Route path="/reits/:ticker" element={<DetailPage />} />
-
-          {/* Crowdfunding list route */}
           <Route path="/Crowdfunding" element={<CrowdfundingPage />} />
-
-          {/* Crowdfunding detail route */}
           <Route path="/Crowdfunding/:vehicle" element={<RecDetailPage />} />
         </Routes>
       </Router>
